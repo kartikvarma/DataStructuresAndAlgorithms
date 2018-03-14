efdfbd995f898cee5ab57b9992bcede2605969b5
kartik <kartikbvarma@icloud.com>
2018-03-14 19:58:38 -0400

    Adding additional methods to the levelordertraversal

--- JAVA/LevelOrderTraversal.java
+++ JAVA/LevelOrderTraversal.java
@@ -1,6 +1,6 @@
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Queue;
+import java.util.Stack;
 
 public class LevelOrderTraversal {
 
@@ -13,13 +13,19 @@ public class LevelOrderTraversal {
     bst.insert(5);
     bst.insert(7);
     bst.insert(9);
-
-    bst.print();
+    System.out.println("Printing in natural order\n");
+    bst.print(false);
+    System.out.println("\n\n");
+    System.out.println("Printing in reverse order\n");
+    bst.print(true);
+    System.out.println("\n\n");
 
     System.out.println("Minimum value in Tree : " + bst.findMin());
     System.out.println("Maximum value in Tree : " + bst.findMax());
     System.out.println("Find value 10 in Tree : " + bst.search(10));
     System.out.println("Find value 7 in Tree : " + bst.search(7));
+    System.out.println("Size of Tree is : " + bst.size());
+    System.out.println("Height of Tree is : " + bst.height());
   }
 }
 
@@ -28,7 +34,9 @@ class Node<T> {
   Node<T> left = null;
   Node<T> right = null;
 
-  Node(T data, Node<T> left, Node<T> right) {
+  public Node() {}
+
+  public Node(T data, Node<T> left, Node<T> right) {
     this.data = data;
     this.left = left;
     this.right = right;
@@ -41,6 +49,12 @@ class BinarySearchTree<T extends Comparable<T>> {
 
   public BinarySearchTree() { this.root = null; }
 
+  /**
+   * Insert an element into tree
+   * when value greater then root add to the right sub tree
+   * when value lesser then root add to the left sub tree
+   * @param value - value of the expected type.
+   */
   public void insert(T value) { root = insert(root, value); }
 
   private Node<T> insert(Node<T> root, T value) {
@@ -54,13 +68,22 @@ class BinarySearchTree<T extends Comparable<T>> {
     return root;
   }
 
-  public void print() {
+  /**
+   *  print all the values in a tree
+   *  @param reverse - when true will print the values in reverse order
+   */
+  public void print(boolean reverse) {
     Queue<Node<T>> queue = new ArrayDeque<>();
+    Stack<T> stack = new Stack<>();
     Node<T> temp = null;
     queue.add(root);
     while (!queue.isEmpty()) {
       temp = queue.remove();
-      System.out.println(temp.data);
+      if (!reverse) {
+        System.out.println(temp.data);
+      } else {
+        stack.add(temp.data);
+      }
 
       if (temp.left != null) {
         queue.add(temp.left);
@@ -70,8 +93,14 @@ class BinarySearchTree<T extends Comparable<T>> {
         queue.add(temp.right);
       }
     }
+    while (!stack.isEmpty()) {
+      System.out.println(stack.pop());
+    }
   }
 
+  /**
+   * Find minimum value in a Tree
+   */
   public T findMin() {
     T min = null;
     Queue<Node<T>> queue = new ArrayDeque<>();
@@ -95,6 +124,9 @@ class BinarySearchTree<T extends Comparable<T>> {
     return min;
   }
 
+  /**
+   * Find the maximum value in a Tree
+   */
   public T findMax() {
     T max = null;
     Queue<Node<T>> queue = new ArrayDeque<>();
@@ -118,6 +150,9 @@ class BinarySearchTree<T extends Comparable<T>> {
     return max;
   }
 
+  /**
+   * search for an element in Tree
+   */
   public boolean search(T value) {
     Queue<Node<T>> queue = new ArrayDeque<>();
     Node<T> temp = null;
@@ -138,5 +173,62 @@ class BinarySearchTree<T extends Comparable<T>> {
       }
     }
     return false;
+  }
+
+  /**
+   * Size of binary tree
+   */
+  public int size() {
+    int count = 0;
+    Queue<Node<T>> queue = new ArrayDeque<>();
+    Node<T> temp = null;
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      temp = queue.remove();
+
+      count++;
+
+      if (temp.left != null) {
+        queue.add(temp.left);
+      }
+
+      if (temp.right != null) {
+        queue.add(temp.right);
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Height of the binary tree
+   * BFS - Breadth first search in the graphs
+   *
+   */
+  public int height() {
+    int count = 0;
+    Queue<Node<T>> queue = new ArrayDeque<>();
+    Node<T> temp = null;
+    Node<T> end = new Node<>();
+    queue.add(root);
+    queue.add(end);
+    while (!queue.isEmpty()) {
+      temp = queue.remove();
+
+      if (temp == end) {
+        if (!queue.isEmpty()) {
+          queue.add(end);
+        }
+        count++;
+      }
+
+      if (temp.left != null) {
+        queue.add(temp.left);
+      }
+
+      if (temp.right != null) {
+        queue.add(temp.right);
+      }
+    }
+    return count;
   }
 }
